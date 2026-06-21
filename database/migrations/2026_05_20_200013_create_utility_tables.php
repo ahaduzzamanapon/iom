ee9a1f8509cfd6953829ff89453175e367fe5531
MosharafMehedi <mosharafmehedi46@gmail.com>
2026-06-21 10:06:18 +0600

    Last Update

--- database/migrations/2026_05_20_200013_create_utility_tables.php
+++ database/migrations/2026_05_20_200013_create_utility_tables.php
@@ -17,6 +17,7 @@ return new class extends Migration {
             $table->foreignId('course_id')->nullable()->constrained()->nullOnDelete();
             $table->boolean('is_published')->default(true);
             $table->timestamp('published_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
             $table->timestamps();
         });
 
